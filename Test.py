186fe83f5ff10b3ccaa6a8face28f0a3e3ee39ca
ManojS <devappasmanoj@gmail.com>
2023-10-28 19:20:44 +0530

    Minor changes

--- Test.py
+++ Test.py
@@ -4,4 +4,4 @@ import pandas as pd
 inputFile= pd.read_excel(r"InputFile.xlsx", sheet_name='Sheet1')
 
 
-inputFile.to_excel('outputFile.xlsx', index=False)
+inputFile.to_excel('output/outputFile.xlsx', index=False)
